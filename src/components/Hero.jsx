30682b0f9a80ebbe8d039fcd5ca7638bf99679d4
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2023-09-27 17:42:55 +0800

    hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,25 @@
-export default function Hero() {
-  return <section>Hero</section>;
+import React from "react";
+
+export default function Hero({}) {
+  return (
+    <section className="flex w-full justify-between p-2">
+      <section>
+        <h1 className="text-[2.5rem]">Full stack developer</h1>
+        <p className="text-sm text-black/70">
+          Hi, I am a developer with 2 years experience{" "}
+        </p>
+        <section className="flex gap-4">
+          <button className="rounded-lg bg-purple-500 px-4 py-2 text-white hover:bg-purple-700 active:bg-purple-800">
+            Lets Talk
+          </button>
+          <button>Lets Talk</button>
+        </section>
+      </section>
+      <section className="min-w[400px]">
+        <div>
+          <img src="/heroright.png" className="object-cover" alt="dev" />
+        </div>
+      </section>
+    </section>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ export default function Navbar({}) {
     <nav className="flex items-center justify-between p-2">
       <div>Logo</div>
       <ul className="flex text-xs font-light">
-        <li className="px-4 py-2">Services</li>
         <li className="px-4 py-2">About</li>
+        <li className="px-4 py-2">Services</li>
         <li className="px-4 py-2">Portfolio</li>
         <li className="px-4 py-2">Testimonia</li>
       </ul>
