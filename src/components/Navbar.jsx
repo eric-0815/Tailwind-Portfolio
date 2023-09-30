003d542236447e218845d1f9137cca0969c86e4b
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2023-09-30 10:48:36 +0800

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,18 @@ export default function Navbar({}) {
   return (
     <nav className="flex items-center justify-between p-2">
       <div>Logo</div>
-      <ul className="flex text-xs font-light">
+      <ul className="hidden text-xs font-light md:flex">
         <li className="px-4 py-2">About</li>
         <li className="px-4 py-2">Services</li>
         <li className="px-4 py-2">Portfolio</li>
         <li className="px-4 py-2">Testimonia</li>
       </ul>
-      <div className="rounded-lg px-6 py-2 font-light ring-1 ring-black/50">
-        Search🔍
-      </div>
+      <section className="flex gap-4">
+        <div className="rounded-lg px-6 py-2 font-light ring-1 ring-black/50">
+          Search🔍
+        </div>
+        <button className="block md:hidden">☰</button>
+      </section>
     </nav>
   );
 }
