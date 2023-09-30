dc7b2c4955a598be4014b2b0820981339616b6e8
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2023-09-30 10:44:45 +0800

    hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,22 +2,28 @@ import React from "react";
 
 export default function Hero({}) {
   return (
-    <section className="flex w-full justify-between p-2">
-      <section>
-        <h1 className="text-[2.5rem]">Full stack developer</h1>
-        <p className="text-sm text-black/70">
+    <section className="md:justfiy-between md: my-0 flex flex-col items-center p-2 md:flex-row">
+      <section className="justfiy-center md: my-20 flex w-full flex-1 flex-col justify-center gap-4  md:my-0 md:w-5/12 ">
+        <h1 className="px-16 text-center text-[2.5rem] font-semibold leading-[3.rem] md:px-0 md:text-left ">
+          Full stack developer
+        </h1>
+        <p className="px-16 text-center text-sm text-black/70 md:px-0 md:text-left">
           Hi, I am a developer with 2 years experience{" "}
         </p>
-        <section className="flex gap-4">
+        <section className="flex flex-col gap-4 md:flex-row">
           <button className="rounded-2xl bg-purple-500 px-8 py-3 text-white hover:bg-purple-700 active:bg-purple-800">
             Lets Talk
           </button>
-          <button className="px-8 py-3 hover:bg-slate-200">Resume</button>
+          <button className="px-8 py-3 hover:bg-slate-200">Resume ↗</button>
         </section>
       </section>
-      <section className="min-w[400px]">
-        <div>
-          <img src="/heroright.png" className="object-cover" alt="dev" />
+      <section className="w-full md:w-7/12">
+        <div className="h-full w-full">
+          <img
+            src="/heroright.png"
+            className="h-full w-full object-cover"
+            alt="dev"
+          />
         </div>
       </section>
     </section>
